e7fa11348d3feb64b4745faf7b3b1f372a0d5c21
Administrator <lemelting53@gmail.com>
2024-11-16 13:04:12 -0300

    feat: criação dos métodos úteis para auxiliar no desenvolvimento do projeto e manter um padrão.

---
+++ src/ToDo/Util/Methods.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.Util
+{
+    public static class Methods
+    {
+        public static bool IsNull(this object? source)
+        {
+            return source == null;
+        }
+    }
+}

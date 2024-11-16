5b24335e250f7d9c73df06504f108fd402319da4
Administrator <lemelting53@gmail.com>
2024-11-16 13:30:28 -0300

    feat: adição dos escopos dos serviços.

--- src/ToDo/Program.cs
+++ src/ToDo/Program.cs
@@ -1,4 +1,7 @@
 
+using ToDo.Contracts;
+using ToDo.Services;
+
 namespace ToDo
 {
     public class Program
@@ -7,16 +10,16 @@ namespace ToDo
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddScoped<ICategoria, CategoriaService>();
+            builder.Services.AddScoped<ITarefa, TarefaService>();
+            builder.Services.AddScoped<IUsuario, UsuarioService>();
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();

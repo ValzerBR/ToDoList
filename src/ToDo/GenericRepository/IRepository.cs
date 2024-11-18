6c57044ec59b74b1ed3e8f0d0ee8c7c135341182
Administrator <lemelting53@gmail.com>
2024-11-17 22:10:58 -0300

    feat: implementação dos métodos para o repositório genérico

---
+++ src/ToDo/GenericRepository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.GenericRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T Save(T entity);
+        void Delete(T entity);
+        IQueryable<T> GetAll();
+        T? GetById(int id);
+    }
+}
---
+++ src/ToDo/GenericRepository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.GenericRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _ctx;
+        private DbSet<T> _dbSet;
+
+        public Repository(DbContext ctx)
+        {
+            _ctx = ctx;
+            _dbSet = _ctx.Set<T>();
+        }
+
+        public T Save(T entity)
+        {
+            if (_ctx.Entry(entity).State == EntityState.Detached)
+                _dbSet.Add(entity);
+            else
+                _dbSet.Update(entity);
+
+            _ctx.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            _ctx.SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet.AsNoTracking();
+        }
+
+        public T? GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+    }
+}

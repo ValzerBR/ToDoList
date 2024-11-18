ec9bf7482061acec5f534b3658f2fc419d1e3dc7
Administrator <lemelting53@gmail.com>
2024-11-17 22:11:18 -0300

    feat: herança do repositório genérico para o repositório da classe "usuário".

---
+++ src/ToDo/Repository/UsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Context;
+using ToDo.GenericRepository;
+using ToDo.Models;
+
+namespace ToDo.Repository
+{
+    public class UsuarioRepository : Repository<Usuario>
+    {
+        public UsuarioRepository(Context.Context ctx) : base(ctx)
+        {
+        }
+    }
+}

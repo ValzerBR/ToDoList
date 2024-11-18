5d91be161e383c9572d54e1a7d35314885b4e3ce
Administrator <lemelting53@gmail.com>
2024-11-17 22:07:06 -0300

    feat: implementação dos métodos no serviço "usuário".

--- src/ToDo/Services/UsuarioService.cs
+++ src/ToDo/Services/UsuarioService.cs
@@ -1,33 +1,64 @@
 ﻿using ToDo.Contracts;
+using ToDo.Exceptions;
 using ToDo.Models;
+using ToDo.Repository;
+using ToDo.Util;
 
 namespace ToDo.Services
 {
     public class UsuarioService : IUsuario
     {
-        public Usuario Create(Usuario usuario)
+        private readonly UsuarioRepository _usuarioRepository;
+        public UsuarioService(UsuarioRepository usuarioRepository)
         {
-            throw new NotImplementedException();
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public Usuario Create(UsuarioDC usuario)
+        {
+            return _usuarioRepository.Save(new Usuario
+            {
+                Nome = usuario.Nome,
+                Email = usuario.Email
+            });
+        }
+
+        public Usuario Update(UsuarioDC usuario)
+        {
+            //Como eu posso melhorar esse método para que eu não precise ficar settando as propriedades do meu usuário toda vez que eu chamar um Update?
+
+            var usuarioExistente = _usuarioRepository.GetById(usuario.Id);
+
+            if (usuarioExistente.IsNull())
+                throw new BusinessException("Usuário não existe.");
+
+            usuarioExistente.Nome = usuario.Nome;
+            usuarioExistente.Email = usuario.Email;
+
+            return _usuarioRepository.Save(usuarioExistente);
         }
 
         public void Delete(int[] ids)
         {
-            throw new NotImplementedException();
+            foreach(int idUsuario in ids)
+            {
+                Usuario usuario = _usuarioRepository.GetById(idUsuario);
+                if(!usuario.IsNull())
+                    _usuarioRepository.Delete(usuario);
+            }
         }
 
         public Usuario Detail(int id)
         {
-            throw new NotImplementedException();
+            Usuario usuario = _usuarioRepository.GetById(id);
+            return usuario;
         }
 
         public IEnumerable<Usuario> Search()
         {
-            throw new NotImplementedException();
+            return _usuarioRepository.GetAll();
         }
 
-        public Usuario Update(Usuario usuario)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }

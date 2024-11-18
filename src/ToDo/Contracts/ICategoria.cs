46d3a8dc87cef0243a6c87540a941a7b6980e36d
Administrator <lemelting53@gmail.com>
2024-11-17 23:17:07 -0300

    feat: ajustes nos contratos.

--- src/ToDo/Contracts/ICategoria.cs
+++ src/ToDo/Contracts/ICategoria.cs
@@ -1,4 +1,5 @@
-﻿using ToDo.Models;
+﻿using System.Runtime.Serialization;
+using ToDo.Models;
 
 namespace ToDo.Contracts
 {
@@ -9,5 +10,14 @@ namespace ToDo.Contracts
         Categoria Detail(int id);
         void Delete(int[] ids);
         IEnumerable<Categoria> Search();
+    }
+
+    [DataContract]
+    public class CategoriaDC
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nome { get; set; }
     }
 }
--- src/ToDo/Contracts/ITarefa.cs
+++ src/ToDo/Contracts/ITarefa.cs
@@ -1,13 +1,39 @@
-﻿using ToDo.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using ToDo.Models;
 
 namespace ToDo.Contracts
 {
     public interface ITarefa
     {
-        Tarefa Create(Tarefa usuario);
-        Tarefa Update(Tarefa usuario);
+        Tarefa Create(TarefaDC usuario);
+        Tarefa Update(TarefaDC usuario);
         Tarefa Detail(int id);
         void Delete(int[] ids);
         IEnumerable<Tarefa> Search();
+    }
+
+    [DataContract]
+    public class TarefaDC
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Titulo { get; set; }
+        [DataMember]
+        public string Descricao { get; set; }
+        [DataMember]
+        public Status Status { get; set; }
+        [DataMember]
+        public DateTime DataDeCriacao { get; set; }
+        [DataMember]
+        public DateTime? DataDeEncerramento { get; set; }
+        [DataMember]
+        public DateTime DataDeVencimento { get; set; }
+        [DataMember]
+        public int UsuarioId { get; set; }
+        [DataMember]
+        public ICollection<CategoriaDC>? Categorias { get; set; }
     }
 }

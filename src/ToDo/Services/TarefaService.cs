8889672f7085b4e0fed55ad8a126ff3d805c8f96
Administrator <lemelting53@gmail.com>
2024-11-17 23:18:02 -0300

    feat: serviço das tarefas.

--- src/ToDo/Services/TarefaService.cs
+++ src/ToDo/Services/TarefaService.cs
@@ -1,33 +1,84 @@
 ﻿using ToDo.Contracts;
 using ToDo.Models;
+using ToDo.Repository;
+using ToDo.Util;
 
 namespace ToDo.Services
 {
     public class TarefaService : ITarefa
     {
-        public Tarefa Create(Tarefa usuario)
+        private readonly TarefaRepository _tarefaRepository;
+        public TarefaService(TarefaRepository tarefaRepository)
         {
-            throw new NotImplementedException();
+            _tarefaRepository = tarefaRepository;
+        }
+
+        public Tarefa Create(TarefaDC tarefa)
+        {
+            //Resolver o problema de não conseguir criar uma tarefa com uma categoria que já exista no sistema.
+
+            return _tarefaRepository.Save(new Tarefa
+            {
+                Id = tarefa.Id,
+                DataDeCriacao = tarefa.DataDeCriacao,
+                DataDeEncerramento = tarefa.DataDeEncerramento,
+                DataDeVencimento = tarefa.DataDeVencimento,
+                Descricao = tarefa.Descricao,
+                Status = tarefa.Status,
+                Titulo = tarefa.Titulo,
+                UsuarioId = tarefa.UsuarioId,
+                Categorias = tarefa.Categorias?.Select(tarefa => new Categoria
+                {
+                    Id = tarefa.Id == 0 ? 0 : tarefa.Id,
+                    Nome = tarefa.Nome
+                }).ToList()
+            });
+        }
+        public Tarefa Update(TarefaDC tarefa)
+        {
+            var tarefaEntity = _tarefaRepository.GetById(tarefa.Id);
+
+            if (tarefaEntity.IsNull())
+                throw new Exception("Tarefa não encontrada");
+
+            tarefaEntity.DataDeEncerramento = tarefa.DataDeEncerramento;
+            tarefaEntity.DataDeVencimento = tarefa.DataDeVencimento;
+            tarefaEntity.Descricao = tarefa.Descricao;
+            tarefaEntity.Status = tarefa.Status;
+            tarefaEntity.Titulo = tarefa.Titulo;
+            tarefaEntity.UsuarioId = tarefa.UsuarioId;
+
+            // Se Categorias forem passadas, atualiza as categorias
+            if (tarefa.Categorias != null)
+            {
+                tarefaEntity.Categorias = tarefa.Categorias.Select(c => new Categoria
+                {
+                    Id = c.Id,
+                    Nome = c.Nome
+                }).ToList();
+            }
+
+            return _tarefaRepository.Save(tarefaEntity);
         }
 
         public void Delete(int[] ids)
         {
-            throw new NotImplementedException();
+            foreach (int idTarefa in ids)
+            {
+                Tarefa tarefa = _tarefaRepository.GetById(idTarefa);
+                if (!tarefa.IsNull())
+                    _tarefaRepository.Delete(tarefa);
+            }
         }
 
         public Tarefa Detail(int id)
         {
-            throw new NotImplementedException();
+            return _tarefaRepository.GetById(id);
         }
 
         public IEnumerable<Tarefa> Search()
         {
-            throw new NotImplementedException();
-        }
-
-        public Tarefa Update(Tarefa usuario)
-        {
-            throw new NotImplementedException();
+            return _tarefaRepository.GetAll().ToList();
         }
     }
 }

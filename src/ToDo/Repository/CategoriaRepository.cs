1d08df04c9d2191edf80089e1b959252b36cbca8
Administrator <lemelting53@gmail.com>
2024-11-19 00:14:32 -0300

    feat: cria repositório para categoria e faz um override no metodo de delete para o repositório de tarefa.

---
+++ src/ToDo/Repository/CategoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Context;
+using ToDo.GenericRepository;
+using ToDo.Models;
+
+namespace ToDo.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>
+    {
+        public CategoriaRepository(Context.Context ctx) : base(ctx)
+        {
+        }
+    }
+}
--- src/ToDo/Repository/TarefaRepository.cs
+++ src/ToDo/Repository/TarefaRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ToDo.Context;
+using ToDo.Contracts;
 using ToDo.GenericRepository;
 using ToDo.Models;
 
@@ -7,8 +8,33 @@ namespace ToDo.Repository
 {
     public class TarefaRepository : Repository<Tarefa>
     {
+
+        private readonly Context.Context _context;
         public TarefaRepository(Context.Context ctx) : base(ctx)
         {
+            _context = ctx;
+        }
+
+        public override void Delete(Tarefa tarefa)
+        {
+            _dbSet.Remove(tarefa);
+
+            var categorias = tarefa.Categorias.ToList();
+            _context.SaveChanges();
+
+            foreach (var categoria in categorias)
+            {
+                var tarefas = _context.Tarefas.Count();
+
+                bool tt = _context.Tarefas.Where(w => w.Id == categoria.Id).Any();
+                var outrasTarefasComEssaCategoria = _context.Tarefas
+                                                             .Any(w => w.Id == categoria.Id);
+
+                if (!outrasTarefasComEssaCategoria)
+                    _context.Categorias.Remove(categoria);
+
+                _context.SaveChanges();
+            }
         }
     }
 }

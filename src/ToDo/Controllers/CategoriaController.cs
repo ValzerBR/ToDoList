63bf534594196d977ca858cfb7a6d1da2bca39a1
Administrator <lemelting53@gmail.com>
2024-11-19 00:12:34 -0300

    feat: criação de uma controller para categoria.

--- src/ToDo/Controllers/CategoriaController.cs
+++ src/ToDo/Controllers/CategoriaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ToDo.Contracts;
 using ToDo.Models;
+using ToDo.Repository;
 using static ToDo.Util.Controller;
 
 namespace ToDo.Controllers
@@ -9,6 +10,31 @@ namespace ToDo.Controllers
     [Route("[controller]")]
     public class CategoriaController : Controller
     {
+        private readonly ICategoria _categoriaContract;
+        public CategoriaController(ICategoria categoriaContract)
+        {
+            _categoriaContract = categoriaContract;
+        }
+
+        [HttpGet("Detail/Categoria/{id}")]
+        public IActionResult Detail(int id)
+        {
+            return JsonOptionsUtil.Create(_categoriaContract.Detail(id));
+        }
+
+        [HttpDelete("Delete/Categoria/")]
+        public IActionResult Delete([FromQuery] int[] ids)
+        {
+            _categoriaContract.Delete(ids);
+            return Json(null);
+        }
+
+        [HttpDelete("Delete/CategoriaDeTarefa/")]
+        public IActionResult DeleteCategoriaFromTarefa([FromQuery] int tarefaId, [FromQuery] int categoriaId)
+        {
+            _categoriaContract.DeleteCategoriaFromTarefa(tarefaId, categoriaId);
+            return Json(null);
+        }
 
     }
 }

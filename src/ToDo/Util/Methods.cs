bd150ad78c8a0c0b8f93778160bb1d18171d14ea
Administrator <lemelting53@gmail.com>
2024-11-19 00:14:47 -0300

    feat: cria método para formatação de datas e retorno de enumeradores.

--- src/ToDo/Util/Methods.cs
+++ src/ToDo/Util/Methods.cs
@@ -1,10 +1,29 @@
-﻿namespace ToDo.Util
+﻿using System.Runtime.CompilerServices;
+
+namespace ToDo.Util
 {
     public static class Methods
     {
         public static bool IsNull(this object? source)
         {
             return source == null;
+        }
+
+        //sobrecarga
+        public static string ToDateBR(this DateTime? data)
+        {
+            return data.HasValue ? data.Value.ToString("dd/MM/yyyy") : string.Empty;
+        }
+
+        public static string ToDateBR(this DateTime data)
+        {
+            return data.ToString("dd/MM/yyyy");
+        }
+
+        // Método de extensão genérico que formata o nome de um valor enum
+        public static string GetEnumName<TEnum>(this TEnum enumValue) where TEnum : Enum
+        {
+            return Enum.GetName(typeof(TEnum), enumValue);
         }
     }
 }
